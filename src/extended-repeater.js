80a34126c50b2d149d63b097dbdc042dc217341a
ibnvladimir <iidesign@yandex.ru>
2020-09-21 02:15:59 +0400

    feat: dine

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
   
+  let repeat = options.repeatTimes || 1;
+  let separator = options.separator || '+';
+  let addRepeat = options.additionRepeatTimes || 1;
+  let addSeparator = options.additionSeparator || '|';
+  let add;
+  if (String(options.addition) == 'null' ||
+      String(options.addition) ==  'false'){
+    add = String(options.addition);
+  } else {
+    add = options.addition || '';
+  };
+
+  str += agregator(add, addRepeat, addSeparator);
+  return agregator(str, repeat, separator);
+};
+
+function agregator(str, number, separator) {
+  let arr = []
+  for (let i = 0; i < number; i++) {
+    if (typeof(str) !== 'string') {
+      str = str + '';
+    }
+    arr[i] = str;
+  }
+  return arr.join(separator);
+}
