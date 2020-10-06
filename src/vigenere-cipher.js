cac97c70e93df97618a42122bd6a8c58bc626e1d
ibnvladimir <iidesign@yandex.ru>
2020-10-06 20:48:05 +0400

    feat: cipher done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,85 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(directionForvard = true) {
+    this.isDirectionForvard = directionForvard;
+    this.template = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.result = [];
+    this.source = [];
+    this.key = [];
   }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error();
+
+    this.prepare(message, key);
+
+    for (let i = 0, j = 0; i < this.source.length; i++) {
+      if (!this.key[j]) j = 0;
+
+      if (this.template.includes(this.source[i])) {
+        let sourceIndex = this.template.indexOf(this.source[i]);
+        let keyIndex = this.template.indexOf(this.key[j]);
+        let resultIndex = (sourceIndex + keyIndex) % 26;
+        this.result.push(this.template[resultIndex]);
+        j++;
+      } else {
+        this.result.push(this.source[i]);
+      }
+    }
+
+    let result = (this.isDirectionForvard) ? this.result.join('') : this.result.reverse().join('');
+    this.clearObject();
+    return result;
+  }
+
+
+
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error();
+
+    this.prepare(message, key);
+
+    for (let i = 0, j = 0; i < this.source.length; i++) {
+      if (!this.key[j]) j = 0;
+
+      if (this.template.includes(this.source[i])) {
+        let sourceIndex = this.template.indexOf(this.source[i]);
+        let keyIndex = this.template.indexOf(this.key[j]);
+        let resultIndex = (sourceIndex - keyIndex + 26) % 26;
+        this.result.push(this.template[resultIndex]);
+        j++;
+      } else {
+        this.result.push(this.source[i]);
+      }
+    }
+
+    let result = (this.isDirectionForvard) ? this.result.join('') : this.result.reverse().join('');
+    this.clearObject();
+    return result;
+   
+  }
+
+  prepare(message, key) {
+    //приводим строку и ключ к верхнему регистру
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    //приводим строку и ключ к массиву
+    key = Array.from(key);
+    message = Array.from(message);
+
+    this.source = message;
+    this.key = key;
+  }
+
+  clearObject(){
+    this.result = [];
+    this.source = [];
+    this.key = [];
+  }
+
 }
 
 module.exports = VigenereCipheringMachine;
